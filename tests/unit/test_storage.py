4aa1a5e6361503df154d97fbfc6197b6e9762ee9
Alexander Kovlev <ak@alkov.pro>
2017-11-08 18:19:58 +0300

    travis ci setup

--- tests/unit/test_storage.py
+++ tests/unit/test_storage.py
@@ -5,9 +5,6 @@ from django_rediser import RedisStorage
 
 
 class TestParser:
-    @pytest.fixture(scope='class', autouse=True)
-    def init(self):
-        self.rs = RedisStorage()
-
     def test__init(self):
-        assert self.rs.connect()
+        rs = RedisStorage()
+        assert rs.connect()
