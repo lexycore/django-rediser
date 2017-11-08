630b49ffd95ddc5b82f0cb0f82c9540d168da382
Alexander Kovlev <ak@alkov.pro>
2017-11-08 19:36:43 +0300

    generate .rst for pypi long_doc

--- setup.py
+++ setup.py
@@ -28,6 +28,47 @@ if travis:
     with open('django_rediser/config.py', 'w', encoding="utf-8") as f:
         f.write("__version__ = '{}'".format(version))
 
+try:
+    import pypandoc
+
+    print("Converting README...")
+    long_description = pypandoc.convert('README.md', 'rst')
+    if branch:
+        long_description = long_description.replace('django-rediser.svg?branch=master', 'django-rediser.svg?branch={}'.format(branch))
+    index_begin = long_description.find('\n*Index:*')
+    index_end = long_description.find('\n<a name="Chapter_1">')
+    examples = long_description.find('\n<a name="Chapter_4">')
+    links = min((long_description.find('\n.. |build'),
+                 long_description.find('\n.. |codacy'),
+                 long_description.find('\n.. |pypi'),
+                 long_description.find('\n.. |license'),
+                 ))
+    if all((index_begin >= 0,
+            index_end >= 0,
+            examples >= 0,
+            links >= 0,
+            )):
+        long_description = '{}{}More information here: {}\n{}'.format(
+            long_description[:index_begin],
+            long_description[index_end:examples + 1],
+            url,
+            long_description[links:].replace('\n', '').replace('.. |', '\n.. |'),
+        )  # .replace('\r\n', '\n')
+
+except (IOError, ImportError, OSError):
+    print("Pandoc not found. Long_description conversion failure.")
+    with open('README.md', encoding="utf-8") as f:
+        long_description = f.read()
+else:
+    print("Saving README.rst...")
+    try:
+        if len(long_description) > 0:
+            with open('README.rst', 'w', encoding="utf-8") as f:
+                f.write(long_description)
+            if travis:
+                os.remove('README.md')
+    except:
+        print("  failed!")
 
 setup(
     name='django-rediser',
@@ -64,6 +105,7 @@ setup(
     ],
     setup_requires=[
         'wheel',
+        'pypandoc',
     ],
     tests_require=[
         'pytest',
